33b30928861596708a333b924a20dde5aea0e9d8
zaynab <zaynabghannam@outlook.com>
2020-07-18 20:30:45 +0200

    Create React app and Convert Html pages into React Components(theatre,scene,theatreMonth,header and footer)

---
+++ theatre-app/src/components/footer.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class footer extends Component {
+    render() {
+        return (
+            <div>
+                {/* footer */}
+                <footer className="page-footer font-small blue">
+                    {/* Copyright */}
+                    <div className="footer-copyright text-center py-3">جميع الحقوق محفوظة @2020
+          <a href="index.html"> مسرح كل شي بوقتو حلو</a>
+                    </div>
+                    {/* Copyright */}
+                </footer>
+                {/* end footer */}
+            </div>
+        );
+    }
+}
+
+export default footer;
