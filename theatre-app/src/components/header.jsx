5e5c6b6ea6a089e0b2eaac53282d1bf94356c572
zaynab <zaynabghannam@outlook.com>
2020-08-12 20:00:18 +0200

    React: Highlighting active link

--- theatre-app/src/components/header.jsx
+++ theatre-app/src/components/header.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Logo from '../img/logo.png'
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 class hello extends Component {
     render() {
@@ -8,23 +8,23 @@ class hello extends Component {
             <div>
                 {/* navbar component */}
                 <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                    {/* <Link className="navbar-brand" to="index.html"><img src={Logo} className="logo" alt="logo" /></Link> */}
+                    {/* <NavLink className="navbar-brand" to="index.html"><img src={Logo} className="logo" alt="logo" /></NavLink> */}
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon" />
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav mr-auto">
                             <li className="nav-item active">
-                                <Link className="nav-link" to="/">الرئيسية <span className="sr-only">(current)</span></Link>
+                                <NavLink className="nav-link" to="/" exact activeClassName="active-link">الرئيسية <span className="sr-only">(current)</span></NavLink>
                             </li>
                             <li className="nav-item">
-                                <Link className="nav-link" to="/all">المسارح</Link>
+                                <NavLink className="nav-link" to="/all"  exact activeClassName="active-link">المسارح</NavLink>
                             </li>
                             <li className="nav-item">
-                                <Link className="nav-link" to="/add">اضافة مسرح</Link>
+                                <NavLink className="nav-link" to="/add"  exact activeClassName="active-link">اضافة مسرح</NavLink>
                             </li>
                             <li className="nav-item active">
-                                <Link className="nav-link" to="/">حول التطبيق <span className="sr-only">(current)</span></Link>
+                                <NavLink className="nav-link" to="/about"  exact activeClassName="active-link">حول التطبيق <span className="sr-only">(current)</span></NavLink>
                             </li>
                         </ul>
                         {/* search */}
